802d4283d28b41b78e021a2ae2c3d4dac852c50d
Deepak Verma <deepak@moresimple.us>
2024-05-06 05:57:36 -0700

    Exmaples 303.4 While Loops Example

---
+++ Jave Classwork/Java Classwork/src/main/java/org/example/Slides_303_4/WhileLoopsExample.java
@@ -0,0 +1,11 @@
+package org.example.Slides_303_4;
+
+public class WhileLoopsExample {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x < 10) {
+            x = x + 1;
+            System.out.println(x);
+        }
+    }
+}
