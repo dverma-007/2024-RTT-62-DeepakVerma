bbbc1cd77670222ce44df19ff8d26bb407dcc78f
Deepak Verma <deepak@moresimple.us>
2024-05-09 07:18:20 -0700

    Finished HW

---
+++ Jave Classwork/Java Classwork/src/main/java/org/example/GLAB_303_3_1/StringUpperExample.java
@@ -0,0 +1,8 @@
+package org.example.GLAB_303_3_1;
+
+public class StringUpperExample {
+    static public void main(String[] args){
+        String s1 = "hello how are you";
+        System.out.println(s1.toUpperCase());
+    }
+}
