1249e2a3ae5e88ac0dc3de41427849789ac16097
Deepak Verma <deepak@moresimple.us>
2024-05-09 07:18:37 -0700

    Finished this HW

---
+++ Jave Classwork/Java Classwork/src/main/java/org/example/GLAB_303_3_1/toLowerExample.java
@@ -0,0 +1,10 @@
+package org.example.GLAB_303_3_1;
+
+public class toLowerExample {
+    public static void main(String[] args){
+        String s1 = "Hello how are you";
+        String s1lower=s1.toLowerCase();
+        System.out.println(s1lower);
+        System.out.println(s1.toLowerCase());
+    }
+}
